c2355c2901e1d1c6de2049467038bf7fc8fdd740
mariakrishna <mkadonai@gmail.com>
2022-05-18 01:08:53 -0300

    selecionando elementos 1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//DOM = Document Object Model
-//tudo que houver na pagina, inclusive sua propria estrutura
-//referencia a cada um dos elementos que aparecem na pagina
-//Virtual DOM fica na memória
+//selecionando elementos na tela
+//a partir do id
+document.getElementById("exemplo").innerHTML =
+  "I love Levihan <button>Yes</button>";
