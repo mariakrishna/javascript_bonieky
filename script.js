8320c1650b44b71c11ee9d5301fdd7e9ad5651c0
mariakrishna <mkadonai@gmail.com>
2022-05-18 00:06:55 -0300

    output

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("Tô funcionando!");
+// quatro formas principais de mostrar dados para o usuário final.
+// 1. manipulando elementos do html através do id
+document.getElementById("titulo").innerHTML = "It's Show Time";
+// 2. criando um elemento novo na tela - aleteração na página
+document.write("Never give up!");
+// 3. utilizando a janela do navegador. pagina (document) =/= de janela do navegador
+window.alert("Writing on the window");
+// 4. console - mostra mensagens // mostra mensagens para o desenvolvedor, não para o usuário final
+console.log("my menssage on the console");
