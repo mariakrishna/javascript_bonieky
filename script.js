9447c858822ac6e168486c67d2d85a9b8dc35e9a
mariakrishna <mkadonai@gmail.com>
2022-05-18 00:27:25 -0300

    condicionais

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-// variáveis = armazenam valores
-// declaração de variáveis
-
-//1. não pode ter caracteres espeicias, não pode começar com números
-var idade = 31;
-// podem ser definidas várias variáveis
-var nome = "Hange"; //palavras sem aspas não são escritas pois o javascript a interpreta como variáveis
-document.write(idade);
-document.write(nome);
-
-var x = 10;
-var y = 15;
-var total = x + y + " ";
-// ele reconhece operações matemáticas, sendo as variáveis numeros
-document.write(total) + " ";
-var nome = "Hange";
-var sobrenome = "Zoë";
-var nomecompleto = nome + " " + sobrenome;
-alert(nomecompleto);
+//condicionais
+//caixas de códigos
+//var hora = 9;
+//dentro do parênteses vai a condição
+//if ( hora == 12 ) o igual é isolado é definição de variavel então para condicionais se usa duplo
+var hora = 15;
+//o código só roda se a condição for satisfeita
+if (hora < 12) {
+  console.log("Bom dia");
+} else {
+  console.log("Não é de manhã");
+}
