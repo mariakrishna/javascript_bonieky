12137638d7b7ac38a436c02ddee7dcf38649f3ae
mariakrishna <mkadonai@gmail.com>
2022-05-18 00:19:46 -0300

    variáveis

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-// quatro formas principais de mostrar dados para o usuário final.
-// 1. manipulando elementos do html através do id
-document.getElementById("titulo").innerHTML = "It's Show Time";
-// 2. criando um elemento novo na tela - aleteração na página
-document.write("Never give up!");
-// 3. utilizando a janela do navegador. pagina (document) =/= de janela do navegador
-window.alert("Writing on the window");
-// 4. console - mostra mensagens // mostra mensagens para o desenvolvedor, não para o usuário final
-console.log("my menssage on the console");
+// variáveis = armazenam valores
+// declaração de variáveis
+
+//1. não pode ter caracteres espeicias, não pode começar com números
+var idade = 31;
+// podem ser definidas várias variáveis
+var nome = "Hange"; //palavras sem aspas não são escritas pois o javascript a interpreta como variáveis
+document.write(idade);
+document.write(nome);
+
+var x = 10;
+var y = 15;
+var total = x + y + " ";
+// ele reconhece operações matemáticas, sendo as variáveis numeros
+document.write(total) + " ";
+var nome = "Hange";
+var sobrenome = "Zoë";
+var nomecompleto = nome + " " + sobrenome;
+alert(nomecompleto);
