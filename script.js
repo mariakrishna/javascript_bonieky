45d0e2e793c2b4647c3ce314182df6eda39149f2
mariakrishna <mkadonai@gmail.com>
2022-05-18 00:59:58 -0300

    DOM

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-// outras definições de variávies
-// variações criadas em 2015
-
-var nome = "Maria Krishna";
-//variável com hoisting - é enviada para o escopo geral da tela e é usada com variaveis globais como a window
-let name = "Levi Ackerman";
-//acessada pelo escopo direto da página. a variável não pode ser redefinida
-const namae = "Hange Zoë";
-//variável com valor constante. não pode receber um novo valor, com excessão de arreys e objetos
-console.log(window.nome);
-console.log(window.name);
-
-if (nome == "Maria Krishna") {
-  var idade = 30;
-  console.log(idade);
-}
+//DOM = Document Object Model
+//tudo que houver na pagina, inclusive sua propria estrutura
+//referencia a cada um dos elementos que aparecem na pagina
+//Virtual DOM fica na memória
