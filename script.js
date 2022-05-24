4a205394eb1c77ed72d21f80e3809f04c4829143
mariakrishna <mkadonai@gmail.com>
2022-05-24 01:28:27 -0300

    selecionando elementos: quatro funções principais: getElementBy...

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 //selecionando elementos na tela
 //a partir do id
 document.getElementById("exemplo").innerHTML =
-  "I love Levihan <button>Yes</button>";
+  "I love Levihan <button>Yes</button>"; //retorna um elemento único pq o id é único
+document.getElementsByClassName("lista")[1].innerHTML = "Levihan Canon"; //sempre retorna um arrey, mesmo que com só um elemento
+document.getElementsByTagName("button")[1].innerHTML = "Levi x Hans";
+document.getElementsByName("telefone");
