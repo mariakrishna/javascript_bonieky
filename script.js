2246a91eec712f35ca8da5ff2cf036123e75d2d9
mariakrishna <mkadonai@gmail.com>
2022-05-25 22:39:37 -0300

    selecionando com queryselector

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-//selecionando elementos na tela
-//a partir do id
-document.getElementById("exemplo").innerHTML =
-  "I love Levihan <button>Yes</button>"; //retorna um elemento único pq o id é único
-document.getElementsByClassName("lista")[1].innerHTML = "Levihan Canon"; //sempre retorna um arrey, mesmo que com só um elemento
-document.getElementsByTagName("button")[1].innerHTML = "Levi x Hans";
-document.getElementsByName("telefone");
+//seleciona por id, class, div, tag etc
+document.querySelector("#exemplo").innerHTML = "<button>126</button>";
