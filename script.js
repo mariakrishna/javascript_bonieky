96b12c73db79f3cabeab021caf3aa89b2a801a75
mariakrishna <mkadonai@gmail.com>
2022-05-25 23:04:08 -0300

    manipulando classes de elementos

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-//seleciona por id, class, div, tag etc
-document.querySelector("#exemplo").innerHTML = "<button>126</button>";
+function canon() {
+  document.querySelector("#exemplo").classList.remove("semicanon");
+  document.querySelector("#exemplo").classList.remove("not-canon");
+  document.querySelector("#exemplo").classList.add("canon");
+}
+
+function semicanon() {
+  document.querySelector("#exemplo").classList.remove("canon");
+  document.querySelector("#exemplo").classList.remove("not-canon");
+  document.querySelector("#exemplo").classList.add("semicanon");
+}
+
+function notcanon() {
+  document.querySelector("#exemplo").classList.remove("semicanon");
+  document.querySelector("#exemplo").classList.remove("canon");
+  document.querySelector("#exemplo").classList.add("not-canon");
+}
+
+function trocar() {
+  if (document.querySelector(".semicanon").classList.contains("semicanon")) {
+    document.querySelector(".semicanon").classList.remove("semicanon");
+    document.querySelector(".semicanon").classList.add("canon");
+  }
+
+  if (document.querySelector(".semicanon").classList.contains("canon")) {
+    document.querySelector(".semicanon").classList.remove("canon");
+    document.querySelector(".semicanon").classList.add("semicanon");
+  }
+}
