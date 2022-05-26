6bcf1bf1b28981bf0f8d3288e76b789d8df4ea4e
mariakrishna <mkadonai@gmail.com>
2022-05-26 00:24:51 -0300

    dimensões

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-function trocarImagem(filename, animalName) {
-  document.querySelector(".img").setAttribute("src", "img/" + filename);
-  // insere um valor em um atributo
-  document.querySelector(".img").setAttribute("data-animal", animalName);
-}
-function pegarAnimal() {
-  let animal = document.querySelector(".img").getAttribute("data-animal");
-  alert("the animal is " + animal);
-}
+document.querySelector("texto").offsetWidth; //leva em conta não apenas o conteudo, mas o padding, a border, e a margin
+document.querySelector("texto").offsetHeight;
+
+document.querySelector("texto").clientWidth; //pega apenas a largura do contudo, excluindo o scroll bar e o padding
+
+document.querySelector("texto").scrollHeight; //pega o tamanho real do conteudo, no caso do exemplo, o texto. tamanho real do conteúdo
