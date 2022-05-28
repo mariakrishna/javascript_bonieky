e93343988baf07b1bd2884a19e7f710e529e92fc
mariakrishna <mkadonai@gmail.com>
2022-05-27 21:55:31 -0300

    evento de teclado

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function clicou() {
   //alert("vc clicou no butão :)");
-  document.getElementById("titulo").innerHTML = "tu mudou o título!";
+  //document.getElementById("titulo").innerHTML = "tu mudou o título!";
+}
+
+function digitou(e) {
+  //console.log(e);
+  if (e.keyCode == 13 && e.ctrlKey == true) {
+    //enter
+    let texto = document.getElementById("campo").value;
+    console.log(texto);
+  }
 }
