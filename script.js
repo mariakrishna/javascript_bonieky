0cd5f505c9c7185219436e5c549c818193e62471
mariakrishna <mkadonai@gmail.com>
2022-05-28 01:44:15 -0300

    projeto menu de navegação

--- script.js
+++ script.js
@@ -1,35 +1,14 @@
-//objetos - similar a arrays
+function menuOpen() {
+  let menuArea = document.getElementById("menu-area");
+  if (menuArea.style.width == "200px") {
+    menuArea.style.width = "0px";
+  } else {
+    menuArea.style.width = "200px";
+  }
+}
 
-//array
-let carros = [
-  { nome: "fiat", modelo: "palio" },
-  { nome: "fiat", modelo: "uno" },
-  { nome: "toyota", modelo: "corola" },
-];
-
-console.log(carros);
-console.log(carros[2].modelo);
-
-//objeto - tem que especificar o nome e valor do item q se quer especificar
-let carro = {
-  //proriedades
-  nome: "fiat",
-  modelo: "uno",
-  peso: "800kg",
-  ligar: function () {
-    //console.log("ligando o " + this.modelo);
-    this.ligar = true;
-    console.log("vrum vrum");
-  },
-  acelerar: function () {
-    if (this.ligar == true) {
-      console.log("trrrrrrrr");
-    } else {
-      console.log(this.nome + " " + this.modelo + " não está ligado");
-    }
-  },
-};
-
-console.log(carro["modelo"]);
-console.log(carro.nome);
-carro.acelerar();
+/*if (menuArea.classList.contains("menu-opened") == true) {
+    menuArea.classList.remove("menu-opened");
+  } else {
+    menuArea.classList.add("menu-opened");
+  }*/
