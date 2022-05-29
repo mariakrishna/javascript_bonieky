c86dca3877226738b525de504d57473684e36230
mariakrishna <mkadonai@gmail.com>
2022-05-29 20:28:01 -0300

    switch

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-function menuOpen() {
-  let menuArea = document.getElementById("menu-area");
-  if (menuArea.style.width == "200px") {
-    menuArea.style.width = "0px";
-  } else {
-    menuArea.style.width = "200px";
-  }
+//switch - um valor com varias condicionais diferentes
+
+let dia = 3;
+let diaNome = "";
+switch (dia) {
+  case 1:
+    diaNome = "monday";
+    break;
+  case 2:
+    diaNome = "tuesday";
+    break;
+  case 3:
+    diaNome = "wednesday";
+    break;
+  case 4:
+    diaNome = "thrusday";
+    break;
+  case 5:
+    diaNome = "friday";
+    break;
+  case 6:
+    diaNome = "saturday";
+    break;
+  case 7:
+    diaNome = "sunday";
+    break;
 }
 
-/*if (menuArea.classList.contains("menu-opened") == true) {
-    menuArea.classList.remove("menu-opened");
-  } else {
-    menuArea.classList.add("menu-opened");
-  }*/
+document.getElementById("dia").innerHTML = "today is: " + diaNome;
